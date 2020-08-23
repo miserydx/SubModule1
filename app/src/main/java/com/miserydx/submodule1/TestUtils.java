2ec65c117e67abad5bce7c289610fcd0274fd6af
miserydx <miserydx@163.com>
2020-08-23 21:16:19 +0800

    添加一个工具类

---
+++ app/src/main/java/com/miserydx/submodule1/TestUtils.java
@@ -0,0 +1,15 @@
+package com.miserydx.submodule1;
+
+import android.util.Log;
+
+/**
+ * Created by miserydx on 2020/8/23.
+ */
+
+public class TestUtils {
+
+    public static void testFun() {
+        Log.d("misery", "test");
+    }
+
+}
